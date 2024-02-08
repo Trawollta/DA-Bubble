fb26dd0e36bf44d23d79e1f2a8fb7a63da2eb579
Systemfehler2701 <Marnie.Lindenthal@web.de>
2024-02-08 09:51:01 +0100

    added fonts and components

---
+++ src/app/inputfield/inputfield.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputfield',
+  standalone: true,
+  imports: [],
+  templateUrl: './inputfield.component.html',
+  styleUrl: './inputfield.component.scss'
+})
+export class InputfieldComponent {
+
+}

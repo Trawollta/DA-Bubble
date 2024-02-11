54f316847d5d1f6266cc1bf301c640444e1a4998
Dominik Mäder <dominik.maeder.97@web.de>
2024-02-11 16:12:27 +0100

    fixed issue when openening page -- issue was on main index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule {}

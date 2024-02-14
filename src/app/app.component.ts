1384b65e7f42e32acb5db99efe2c4c1846d5b381
Systemfehler2701 <Marnie.Lindenthal@web.de>
2024-02-14 20:26:37 +0100

    angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { initializeApp } from 'firebase/app';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

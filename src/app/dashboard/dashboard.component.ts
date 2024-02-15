cb9e5fdf955e57b5de01890230ec604aa0921fc0
LanceDark <dominik.maeder.97@web.de>
2024-02-15 13:11:57 +0100

    added message container + headline

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ChannelMenuComponent } from "./channel-menu/channel-menu.component";
+import { ChatComponent } from "./chat/chat.component";
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
-    imports: [ChannelMenuComponent]
+    imports: [ChannelMenuComponent, ChatComponent]
 })
 export class DashboardComponent {
 

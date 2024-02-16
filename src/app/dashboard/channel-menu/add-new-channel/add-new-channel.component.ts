3b6b4a8febd7d6982dbff61d249a43727a5fba9f
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-02-16 16:00:19 +0100

    JW/changes_in_channel_insert

--- src/app/dashboard/channel-menu/add-new-channel/add-new-channel.component.ts
+++ src/app/dashboard/channel-menu/add-new-channel/add-new-channel.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject} from '@angular/core';
+import { GlobalFunctionsService } from 'app/services/global-functions.service';
+import { GlobalVariablesService } from 'app/services/global-variables.service';
 
 @Component({
   selector: 'app-add-new-channel',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './add-new-channel.component.html',
   styleUrl: './add-new-channel.component.scss'
 })
 export class AddNewChannelComponent {
+
+  globalVariables = inject (GlobalVariablesService);
+  globalFunctions = inject (GlobalFunctionsService);
+  
 
 }
--- src/app/services/global-functions.service.ts
+++ src/app/services/global-functions.service.ts
@@ -6,10 +6,18 @@ import { Injectable } from '@angular/core';
 export class GlobalFunctionsService {
   
   active: boolean = false;
+  channel: boolean =false;
 
   menuClicked() {
     this.active = !this.active;
     if (this.active) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'auto'; 
+
+  }
+
+  openOverlay() {
+    this.channel = !this.channel;
+    if (this.channel) document.body.style.overflow = 'hidden';
     else document.body.style.overflow = 'auto'; 
 
   }

3a89b29d2c6216936a4f9bd31c0658c79d681d77
Systemfehler2701 <Marnie.Lindenthal@web.de>
2024-02-20 10:55:12 +0100

    input and login

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ButtonComponent } from 'app/button/button.component';
 import { InputfieldComponent } from 'app/inputfield/inputfield.component';
 import { GlobalVariablesService } from 'app/services/global-variables.service';
@@ -7,7 +8,7 @@ import { GlobalVariablesService } from 'app/services/global-variables.service';
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [CommonModule, InputfieldComponent, ButtonComponent],
+  imports: [CommonModule, InputfieldComponent, ButtonComponent, FormsModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })

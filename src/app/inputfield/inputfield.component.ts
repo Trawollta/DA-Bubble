310a556bce9207b068166c316671ddfa0405950c
Alex Solms <duaedin@web.de>
2024-02-26 13:44:56 +0100

    bla

--- src/app/inputfield/inputfield.component.ts
+++ src/app/inputfield/inputfield.component.ts
@@ -28,7 +28,7 @@ export class InputfieldComponent implements OnInit, ControlValueAccessor {
   @Input() imgSize: string = "";
   @Input() required: boolean = false;
   imgActive: string = "";
-  @Input() value: string = ''; //hi Marni, muss ich das hier nicht auch dem Inputfeld übergebe? Gruß Alex
+  value: string = ''; 
   private onChange: (value: string) => void = () => { };
   private onTouched: () => void = () => { };
 

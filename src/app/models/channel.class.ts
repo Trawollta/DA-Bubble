2030f6e37e45bad227f503fdfef1c9be3b98cf01
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-03-10 14:40:50 +0100

    JW/User can now selected to channel

--- src/app/models/channel.class.ts
+++ src/app/models/channel.class.ts
@@ -1,11 +1,16 @@
+import { User } from "./user.class";
 export class channel {
 
     channelName: string;
-    memberId: [];
+    description: string;
+    creator: User | null; 
+    members: User[];
 
     constructor(obj?: any) {
         this.channelName = obj ? obj.channelName : '';
-        this.memberId = []; 
+        this.description = obj ? obj.description : '';
+        this.creator = obj && obj.creator ? new User(obj.creator) : null;
+        this.members = obj && obj.members ? obj.members.map((member: any) => new User(member)) : [];
 
     }
 }

23d922e9e39391cb077c414ad932644574f3e1a9
Alex Solms <duaedin@web.de>
2024-03-10 13:59:35 +0100

    Alex: added time bar to chat messages

--- src/app/shared/chats/all-messages/all-messages.component.ts
+++ src/app/shared/chats/all-messages/all-messages.component.ts
@@ -1,41 +1,74 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Input, LOCALE_ID, inject } from '@angular/core';
 import { OtherUserMessageComponent } from 'app/shared/chats/other-user-message/other-user-message.component';
 import { CurrentUserMessageComponent } from 'app/shared/chats/current-user-message/current-user-message.component';
 import { ChatChannel } from 'app/models/chatChannel.class';
 import { ChatUsers } from 'app/models/chatUsers.class';
 import { FirebaseChatService } from 'app/services/firebase-services/firebase-chat.service';
 import { GlobalVariablesService } from 'app/services/app-services/global-variables.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { ReactionsComponent } from "../../reactions/reactions.component";
 import { GlobalFunctionsService } from 'app/services/app-services/global-functions.service';
 
 
 @Component({
-    selector: 'app-all-messages',
-    standalone: true,
-    templateUrl: './all-messages.component.html',
-    styleUrl: './all-messages.component.scss',
-    imports: [
-        CommonModule,
-        OtherUserMessageComponent,
-        CurrentUserMessageComponent,
-        ReactionsComponent
-    ]
+  selector: 'app-all-messages',
+  standalone: true,
+  templateUrl: './all-messages.component.html',
+  styleUrl: './all-messages.component.scss',
+  // providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }],
+  imports: [
+    CommonModule,
+    OtherUserMessageComponent,
+    CurrentUserMessageComponent,
+    ReactionsComponent,
+    DatePipe
+  ]
 })
 export class AllMessagesComponent {
 
-  firebaseChatService = inject (FirebaseChatService);
+  firebaseChatService = inject(FirebaseChatService);
   globalVariablesService = inject(GlobalVariablesService);
-  GlobalFunctionsService = inject(GlobalFunctionsService)
+  GlobalFunctionsService = inject(GlobalFunctionsService);
+
+  //@Input() message: any;
+
   chatChannel: ChatChannel = new ChatChannel;
   chatUsers: ChatUsers = new ChatUsers;
+  postingTime: string | null = null;
 
-   ngOnInit(){
-   // console.log('aktueller chat', this.globalVariablesService.chatChannel);
-  //  console.log('neue ID: ',this.globalVariablesService.openChannel.id);
-   // this.firebaseChatService.getChat(this.globalVariablesService.openChannel.id); //Id vom Willkommenchannel 'NQMdt08FAcXbVroDLhvm'
-   // console.log('aktueller chat 2.Aufruf', this.globalVariablesService.chatChannel);
-   // console.log(this.firebaseChatService.groupMessagesByAnswerTo());
-  } 
+  lastDisplayedDate: Date = new Date();
+
+  ngOnInit() {
+
+     if (this.globalVariablesService.chatChannel.messages.length > 0) {
+      this.lastDisplayedDate = new Date(this.globalVariablesService.chatChannel.messages[0].timestamp);
+    }
+
+  }
+
+  /**
+   * this function returns the weekday in German of the day of the message timestamp
+   * this function needs to reviewed in case other langeuages should be supported
+   * @param timestamp - the timestamp of the message
+   * @returns - weekday as string
+   */
+  getWeekDay(timestamp: number): string {
+    const today = new Date(timestamp).toDateString() == new Date().toDateString();
+    return today? 'Heute' : new Date(timestamp).toLocaleDateString('de-DE', {weekday: 'long'});
+  }
+
+  /**
+   * this function returns false if the previous message has the same date like the current massage
+   * it also overwrites the lastDisplayedDate if displayDate == true
+   * @param messageTimestamp - timestamp of message
+   * @returns - boolean
+   */
+  shouldDisplayDate(messageTimestamp: number): boolean {
+    const displayDate = this.lastDisplayedDate.toLocaleDateString() !== new Date(messageTimestamp).toLocaleDateString();
+    if (displayDate) {
+      this.lastDisplayedDate = new Date(messageTimestamp);
+    }
+    return displayDate;
+  }
 
 }
--- src/app/shared/chats/current-user-message/current-user-message.component.ts
+++ src/app/shared/chats/current-user-message/current-user-message.component.ts
@@ -35,33 +35,45 @@ export class CurrentUserMessageComponent {
   user: User = new User;
 
   unsubUser;
-  userId:string = 'guest';
+  userId: string = 'guest';
 
   constructor() {
     this.unsubUser = this.getUser(this.userId);
-    
+
   }
 
+  /**
+   * this function unsubscribes the containing content
+   */
   ngOnDestroy() {
     this.unsubUser;
   }
 
-
+  /**
+    * thsi function returns the reference to the user doc
+    * @param docId - id of user
+    * @returns - referenz of document
+    */
   getUserRef(docId: string) {
     return doc(collection(this.firestore, 'users'), docId);
   }
 
-
+  /**
+   * this function get data of user and saves it in lokal user object
+   * @param id - id of user
+   * @returns - onSnapshot object
+   */
   getUser(id: string) {
-     onSnapshot(this.getUserRef(id), (user) => {
-      if(user.data()){
+    return onSnapshot(this.getUserRef(id), (user) => {
+      if (user.data()) {
         this.user = new User(user.data());
       }
     });
-    return 
   }
 
-
+  /**
+   * this function calls function getUser() for providing userdata for the post
+   */
   ngOnInit() {
     this.user = this.message.userId;
     this.getUser(this.message.userId);

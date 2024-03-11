54b256ec85360f84b0a8d078067d265e46603f43
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-03-11 22:50:02 +0100

    JW/component will be fixed soon

--- src/app/dashboard/channel-menu/show-contacts/show-contacts.component.ts
+++ src/app/dashboard/channel-menu/show-contacts/show-contacts.component.ts
@@ -1,25 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { ButtonComponent } from 'app/shared/button/button.component';
-import { InputfieldComponent } from 'app/shared/inputfield/inputfield.component';
+import { Component, inject, OnInit } from '@angular/core'; 
 import { GlobalFunctionsService } from 'app/services/app-services/global-functions.service';
 import { GlobalVariablesService } from 'app/services/app-services/global-variables.service';
-import { AddNewChannelComponent } from '../add-new-channel/add-new-channel.component';
-import { FirebaseChatService } from 'app/services/firebase-services/firebase-chat.service';
-import { FirebaseCannelService } from 'app/services/firebase-services/firebase-cannel.service';
-import { Observable, from, map, of, Subject} from 'rxjs';
-import { FirebaseUserService } from 'app/services/firebase-services/firebase-user.service';
-import { User } from 'app/models/user.class';
-import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 import { ChannelMenuComponent } from '../channel-menu.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
-  // Komponenten-Metadaten...
+  selector: 'app-show-contacts',
+  standalone: true,
+  imports: [CommonModule, ChannelMenuComponent],
+  templateUrl: './show-contacts.component.html',
+  styleUrls: ['./show-contacts.component.scss']
 })
 export class ShowContactsComponent implements OnInit {
   allUsers: any = [];
-  firestore = inject(Firestore); // Firestore-Dienst injizieren
 
   globalFunctions = inject(GlobalFunctionsService);
   globalVariables = inject(GlobalVariablesService);
@@ -27,21 +20,7 @@ export class ShowContactsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.loadChannelMembers();
+ 
   }
 
-  async loadChannelMembers() {
-    const memberIds = this.globalVariables.openChannel.members;
-    if (memberIds && memberIds.length > 0) {
-      const usersCollectionRef = collection(this.firestore, 'users');
-      const q = query(usersCollectionRef, where('userId', 'in', memberIds));
-      
-      try {
-        const querySnapshot = await getDocs(q);
-        this.allUsers = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      } catch (error) {
-        console.error("Fehler beim Laden der Benutzerdaten:", error);
-      }
-    }
-  }
 }

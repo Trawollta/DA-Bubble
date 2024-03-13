2f3281512d21e3d80658d99b630edda746664981
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-03-13 13:34:17 +0100

    JW/working on show members overlay

--- src/app/dashboard/channel-menu/show-contacts/show-contacts.component.ts
+++ src/app/dashboard/channel-menu/show-contacts/show-contacts.component.ts
@@ -3,24 +3,32 @@ import { Component, inject, OnInit } from '@angular/core';
 import { GlobalFunctionsService } from 'app/services/app-services/global-functions.service';
 import { GlobalVariablesService } from 'app/services/app-services/global-variables.service';
 import { ChannelMenuComponent } from '../channel-menu.component';
+import { AddContactsComponent } from '../add-contacts/add-contacts.component';
 
 @Component({
   selector: 'app-show-contacts',
   standalone: true,
-  imports: [CommonModule, ChannelMenuComponent],
+  imports: [CommonModule, ChannelMenuComponent, AddContactsComponent],
   templateUrl: './show-contacts.component.html',
   styleUrls: ['./show-contacts.component.scss']
 })
 export class ShowContactsComponent implements OnInit {
-  allUsers: any = [];
+  selectedUserIds: number[] = []; 
+  selectedUsers: any[] = [];
+  allUsers: any[] = []; 
 
   globalFunctions = inject(GlobalFunctionsService);
-  globalVariables = inject(GlobalVariablesService);
 
-  constructor() {}
+  constructor(public globalVariables: GlobalVariablesService) {}
 
   ngOnInit() {
- 
+    this.updateSelectedUsers();
+  }
+
+  updateSelectedUsers() {
+    this.selectedUsers = this.allUsers.filter(user => this.selectedUserIds.includes(user.id));
   }
 
 }
+
+

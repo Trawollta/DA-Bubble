fef5a7320a89a3f116d699e8abf7f31193fc791d
LanceDark <dominik.maeder.97@web.de>
2024-04-03 11:57:03 +0200

    added remove of ids on user when delete chat

--- src/app/services/firebase-services/firebase-channel.service.ts
+++ src/app/services/firebase-services/firebase-channel.service.ts
@@ -1,21 +1,34 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, addDoc, updateDoc, doc, setDoc, onSnapshot, getDoc, arrayUnion } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  setDoc,
+  onSnapshot,
+  getDoc,
+  arrayUnion,
+  where,
+  query,
+  getDocs,
+  deleteDoc,
+} from '@angular/fire/firestore';
 import { GlobalVariablesService } from '../app-services/global-variables.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseChannelService {
-
   firestore: Firestore = inject(Firestore);
   globalVariables = inject(GlobalVariablesService);
 
-  constructor() { }
+  constructor() {}
 
   // async getChannelById(channelId: string): Promise<any> {
   //   const docRef = doc(this.firestore, 'channels', channelId);
   //   const docSnap = await getDoc(docRef);
-    
+
   //   if (docSnap.exists()) {
   //     console.log("Dokumentdaten:", docSnap.data());
   //     return docSnap.data();
@@ -29,8 +42,10 @@ export class FirebaseChannelService {
     console.log(`Aktualisiere Kanal ${channelId} mit `, item);
     const docRef = doc(this.firestore, 'channels', channelId);
     return updateDoc(docRef, item)
-      .then(() => console.log("Daten erfolgreich aktualisiert"))
-      .catch(error => console.error("Fehler beim Aktualisieren der Daten", error));
+      .then(() => console.log('Daten erfolgreich aktualisiert'))
+      .catch((error) =>
+        console.error('Fehler beim Aktualisieren der Daten', error)
+      );
   }
 
   updateChannelName(channelId: string, newName: string) {
@@ -42,13 +57,13 @@ export class FirebaseChannelService {
   }
 
   async loadChannelData(docId: string) {
-    const docRef = doc(this.firestore, 'channels', docId); 
+    const docRef = doc(this.firestore, 'channels', docId);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      console.log("Dokumentdaten:", docSnap.data());
+      console.log('Dokumentdaten:', docSnap.data());
       return docSnap.data();
     } else {
-      console.log("Kein Dokument gefunden!");
+      console.log('Kein Dokument gefunden!');
       return null;
     }
   }
@@ -57,45 +72,84 @@ export class FirebaseChannelService {
     const docRef = doc(this.firestore, 'channels', channelId);
     return onSnapshot(docRef, (doc) => {
       if (doc.exists()) {
-        console.log("Aktuelle Kanaldaten:", doc.data());
+        console.log('Aktuelle Kanaldaten:', doc.data());
         // Behandle die aktualisierten Daten, z.B. durch Aktualisierung des UI
       } else {
-        console.log("Kein Dokument gefunden!");
+        console.log('Kein Dokument gefunden!');
       }
     });
   }
 
-    /**
+  /**
    * this funktion updates the element
    * @param data -Json
    */
-    async updateDataChannel(data: { [x: string]: any }, docId: string) {
-      await updateDoc(doc(this.firestore, 'channels', docId), data);
-    }
+  async updateDataChannel(data: { [x: string]: any }, docId: string) {
+    await updateDoc(doc(this.firestore, 'channels', docId), data);
+  }
 
-
-    updateChannelTitle(channelId: string, newTitle: string): void {
-      // Aktualisiere den Kanal im Backend, zum Beispiel in Firebase oder einer anderen Datenbank
-      this.updateChannel(channelId, { titel: newTitle }).then(() => {
-        // Aktualisiere den lokalen Zustand direkt nach dem erfolgreichen Backend-Update
-        if (this.globalVariables.openChannel && this.globalVariables.openChannel.id === channelId) {
-          this.globalVariables.openChannel.titel = newTitle;
-        }
-      });
-    }
-
-
-    async addUserToChannel(channelId: string, userId: string): Promise<void> {
-      const channelRef = doc(this.firestore, 'channels', channelId);
-      try {
-        await updateDoc(channelRef, {
-          members: arrayUnion(userId)
-        });
-        console.log('Benutzer erfolgreich zum Kanal hinzugefügt');
-      } catch (error) {
-        console.error('Fehler beim Hinzufügen des Benutzers zum Kanal:', error);
+  updateChannelTitle(channelId: string, newTitle: string): void {
+    // Aktualisiere den Kanal im Backend, zum Beispiel in Firebase oder einer anderen Datenbank
+    this.updateChannel(channelId, { titel: newTitle }).then(() => {
+      // Aktualisiere den lokalen Zustand direkt nach dem erfolgreichen Backend-Update
+      if (
+        this.globalVariables.openChannel &&
+        this.globalVariables.openChannel.id === channelId
+      ) {
+        this.globalVariables.openChannel.titel = newTitle;
       }
-    }
-    }
+    });
+  }
 
+  async addUserToChannel(channelId: string, userId: string): Promise<void> {
+    const channelRef = doc(this.firestore, 'channels', channelId);
+    try {
+      await updateDoc(channelRef, {
+        members: arrayUnion(userId),
+      });
+      console.log('Benutzer erfolgreich zum Kanal hinzugefügt');
+    } catch (error) {
+      console.error('Fehler beim Hinzufügen des Benutzers zum Kanal:', error);
+    }
+  }
 
+  async deleteChanel(channelId: string) {
+    const docId = await this.getDocId(channelId);
+    await this.deleteChannelIdFromUsers(channelId);
+    const channelDocRef = doc(this.firestore, 'channels', docId[0]);
+    await deleteDoc(channelDocRef);
+  }
+
+  async getDocId(chatId: string): Promise<string[]> {
+    const usersCollectionRef = collection(this.firestore, 'channels');
+    const q = query(usersCollectionRef, where('chatId', '==', chatId));
+    const querySnapshot = await getDocs(q);
+    const docIds: string[] = [];
+    querySnapshot.forEach((doc) => {
+      docIds.push(doc.id);
+      console.log(doc.id, ' => ', doc.data());
+    });
+    return docIds;
+  }
+
+  async deleteChannelIdFromUsers(chatId : string) {
+    debugger;
+    let usersCollectionRef = collection(this.firestore, 'users');
+    const querySnapshot = await getDocs(usersCollectionRef);
+    querySnapshot.forEach(async (userDoc) => {
+      const userData = userDoc.data();
+      if (
+        userData &&
+        userData['relatedChats'] &&
+        userData['relatedChats'].includes(chatId)
+      ) {
+        const updatedRelatedChats = userData['relatedChats'].filter(
+          (id: any) => id !== chatId
+        );
+        const userDocRef = doc(this.firestore, 'users', userDoc.id);
+        await updateDoc(userDocRef, { relatedChats: updatedRelatedChats });
+        console.log(`Chat ID removed from user ${userDoc.id}`);
+      }
+    });
+  }
+}

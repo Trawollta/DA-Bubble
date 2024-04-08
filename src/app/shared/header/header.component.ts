35e6633842552d927004da0288d2fbe6e396f96b
LanceDark <dominik.maeder.97@web.de>
2024-04-08 13:55:58 +0200

    stopped work on search input await Marnie Feedback

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,6 +4,7 @@ import { HeaderMenuComponent } from './header-menu/header-menu.component';
 import { GlobalVariablesService } from 'app/services/app-services/global-variables.service';
 import { InputfieldComponent } from 'app/shared/inputfield/inputfield.component';
 import { FirebaseUserService } from 'app/services/firebase-services/firebase-user.service';
+import { FirebaseChannelService } from 'app/services/firebase-services/firebase-channel.service';
 
 @Component({
   selector: 'app-header',
@@ -15,8 +16,12 @@ import { FirebaseUserService } from 'app/services/firebase-services/firebase-use
 export class HeaderComponent {
   globalVariables = inject(GlobalVariablesService);
   firebaseUserService = inject(FirebaseUserService);
+  firebaseChannelService = inject(FirebaseChannelService);
   result: any;
   allDataWithCurrentId: any;
+  allChannels: any = [];
+  relatedChats: any = [];
+  allRelatedChatMsgs: any = [];
 
   constructor() {}
   openChannels() {
@@ -26,9 +31,13 @@ export class HeaderComponent {
   }
 
   async handleInputChange(value: string) {
-    let user = await this.getDataConnectedWithID(this.globalVariables.currentUser.name); // this will be all User Data
+    let user = await this.getDataConnectedWithID(
+      this.globalVariables.currentUser.name
+    ); // this will be all User Data
     if (user) {
-      console.log(user['relatedChats']);
+      this.saveRelatedChats(user['relatedChats']);
+      console.log(this.relatedChats);
+      this.searchForWord(value);
     } else {
       console.log('Benutzer nicht gefunden oder fehlerhafte Daten');
     }
@@ -39,5 +48,36 @@ export class HeaderComponent {
     let docID = await this.firebaseUserService.getUserDocIdWithName(id);
     let data = this.firebaseUserService.getUserData(docID[0]);
     return data;
+  }
+
+  async saveRelatedChats(data: any) {
+    this.relatedChats = data;
+  }
+
+  async searchForWord(word: string) {
+    await this.getChats();
+    this.connectChannelWithChannelMsg();
+    console.log(this.allRelatedChatMsgs);
+  }
+
+  async getChats() {
+    for (let i = 0; i < this.relatedChats.length; i++) {
+      let channels = await this.firebaseChannelService.loadChannelData(this.relatedChats[i]);
+      this.allChannels.push(channels)
+    }
+  }
+
+  connectChannelWithChannelMsg() {
+    if (this.allChannels && this.allChannels.length > 0) {
+      let allChannelsId = this.allChannels[0].chatId;
+      this.firebaseChannelService.getConnectionOfChannel(allChannelsId).then(data => {
+        this.allRelatedChatMsgs.push(data);
+      });
+      
+    } else {
+      console.log('Keine Kanäle gefunden oder ungültige Daten');
+    }
+   
+    
   }
 }
